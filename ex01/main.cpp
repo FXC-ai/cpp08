f476f15de44d47742d5205f2e114ba43684cd7da
Francois-xavier Coindreau <fcoindre@student.42.fr>
2023-10-05 23:38:12 +0200

    improvement ti make for addNumbers with distance

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
+#include <limits>
 
 
 class GiveRandNumb
@@ -35,7 +36,7 @@ class Span
 		std::vector<int>	get_datas() const;
 
 		void				addNumber(int n);
-		void				fill(int value);
+		void				addNumbers(std::vector<int>::iterator first, std::vector<int>::iterator last);
 		void				fillRandomly(int max);
 		size_t				size();
 		int					longestSpan();
@@ -90,10 +91,30 @@ void Span::addNumber(int n)
 	this->_datas.push_back(n);
 }
 
-void Span::fill(int value)
+// void Span::fill(int value)
+// {
+// 	this->_datas.resize(this->_N, value);
+// }
+
+void Span::addNumbers(std::vector<int>::iterator first, std::vector<int>::iterator last)
 {
-	this->_datas.resize(this->_N, value);
+
+	std::cout << "DISTANCE = "<< distance(first, last) << std::endl;
+
+
+	// if (this->size() < this->_N)
+	// {
+	// 	for (size_t index = this->size(); index < N; ++index)
+	// 	{
+	// 		this->_datas[index] = *(first + index);
+	// 	}
+	// }
+
+
+
 }
+
+
 
 void Span::fillRandomly(int max)
 {
@@ -150,9 +171,37 @@ int Span::max()
 	return *maxi;
 }
 
+
+class Display
+{
+	public:
+		void operator()(int a) const
+		{
+			std::cout << a << std::endl;
+		}
+
+
+};
+
+
+
+
 int main()
 {
+	int N = 10;
+
+
 	srand(time(NULL));
+	// unsigned int imax = std::numeric_limits<unsigned int>::max();
+	// std::cout << imax << std::endl;
+	std::vector<int> randomNumbers(N);
+
+	GiveRandNumb giveRandN(N);
+
+	std::generate(randomNumbers.begin(), randomNumbers.end(), giveRandN);
+
+	for_each(randomNumbers.begin(), randomNumbers.end(), Display());
+
 	Span span0;
 	Span span1(10);
 
@@ -160,9 +209,7 @@ int main()
 	span1.fillRandomly(10000);
 	//span1.fillRandomly(0);
 
-
-
-
+	span1.addNumbers(randomNumbers.begin(), randomNumbers.end());
 
 	std::cout << "maxi = " << span1.max() << " mini = " << span1.min() << " Distance = " << span1.longestSpan() << std::endl;	// std::cout << span1;
 
