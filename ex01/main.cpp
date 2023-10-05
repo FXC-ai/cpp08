78bad31f52044a064b059732805e86273c5cae6e
Francois-xavier Coindreau <fcoindre@student.42.fr>
2023-10-05 18:01:19 +0200

    ex01 ready for test phase

---
+++ ex01/main.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+
+class GiveRandNumb
+{
+	private :
+		const int _modulo;
+
+	public :
+		GiveRandNumb() : _modulo(0){};
+		GiveRandNumb(const int n) : _modulo(n){};
+		~GiveRandNumb(){};
+
+		int operator()(){return rand() % this->_modulo;};
+
+
+};
+
+
+class Span
+{
+
+	public :
+		Span();
+		Span(unsigned int N);
+		Span(const Span & src);
+		~Span();
+
+		Span & operator=(const Span & rhs);
+		unsigned int 		get_N() const;
+		std::vector<int>	get_datas() const;
+
+		void				addNumber(int n);
+		void				fill(int value);
+		void				fillRandomly(int max);
+		size_t				size();
+		int					longestSpan();
+		int					shortestSpan();
+		int					min();
+		int					max();
+
+	private :
+		std::vector<int>	_datas;
+		unsigned int 			_N;
+
+
+};
+
+Span::Span() : _N(1){}
+
+Span::Span(unsigned int N) : _N(N){}
+
+Span::Span(const Span & src) : _N(src.get_N())
+{
+	this->_datas = src.get_datas();
+}
+
+Span::~Span(){};
+
+Span & Span::operator=(const Span & rhs)
+{
+	if (&rhs != this)
+	{
+		this->_N = rhs.get_N();
+		this->_datas = rhs.get_datas();
+	}
+	return *this;
+}
+
+unsigned int Span::get_N() const
+{
+	return this->_N;
+}
+
+std::vector<int>	Span::get_datas() const
+{
+	return this->_datas;
+}
+
+void Span::addNumber(int n)
+{
+	if (this->_datas.size() == this->_N)
+	{
+		throw std::exception();
+	}
+	this->_datas.push_back(n);
+}
+
+void Span::fill(int value)
+{
+	this->_datas.resize(this->_N, value);
+}
+
+void Span::fillRandomly(int max)
+{
+
+	GiveRandNumb giveRandN(max);
+	
+	if (this->size() < this->_N)
+	{
+		for (size_t index = this->size(); index < this->_N; ++index)
+		{
+			this->addNumber(giveRandN());
+		}
+	}
+	else
+	{
+		generate(this->_datas.begin(), this->_datas.end(), giveRandN);
+	}
+}
+
+size_t Span::size()
+{
+	return this->_datas.size();
+}
+
+int Span::longestSpan()
+{
+	if (this->size() <= 1)
+	{
+		throw std::exception();
+	}
+
+	return this->max() - this->min();
+}
+
+int Span::shortestSpan()
+{
+	if (this->size() <= 1)
+	{
+		throw std::exception();
+	}
+	sort(this->_datas.begin(), this->_datas.end());
+	return this->_datas[1] - this->_datas[0];
+}
+
+int Span::min()
+{
+	std::vector<int>::iterator mini = min_element(this->_datas.begin(), this->_datas.end());
+	return *mini;
+}
+
+int Span::max()
+{
+	std::vector<int>::iterator maxi = max_element(this->_datas.begin(), this->_datas.end());
+	return *maxi;
+}
+
+int main()
+{
+	srand(time(NULL));
+	Span span0;
+	Span span1(10);
+
+	//span1.fill(2);
+	span1.fillRandomly(10000);
+	//span1.fillRandomly(0);
+
+
+
+
+
+	std::cout << "maxi = " << span1.max() << " mini = " << span1.min() << " Distance = " << span1.longestSpan() << std::endl;	// std::cout << span1;
+
+	span1.shortestSpan();
+	for (unsigned int i = 0; i < span1.get_N(); ++i)
+	{
+		std::cout << span1.get_datas()[i] << " ";
+	}
+
+	std::cout<<std::endl;
+
+	std::cout << "shortest span = " << span1.shortestSpan() << std::endl;
+
+	return 0;
+}
